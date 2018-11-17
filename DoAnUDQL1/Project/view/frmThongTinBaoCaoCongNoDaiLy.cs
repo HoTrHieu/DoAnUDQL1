a57ba5097fc41dd1e0e4296ad346b5aaf2ddcce2
hieudona8 <hieudona8@gmail.com>
2018-11-18 00:53:39 +0700

    update sounce -các form giao diện cần làm

---
+++ DoAnUDQL1/Project/view/frmThongTinBaoCaoCongNoDaiLy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project.view
+{
+    public partial class frmThongTinBaoCaoCongNoDaiLy : Form
+    {
+        public frmThongTinBaoCaoCongNoDaiLy()
+        {
+            InitializeComponent();
+        }
+    }
+}

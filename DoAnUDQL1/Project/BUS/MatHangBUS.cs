516bbe471b5e9d1977e4359d3f166911c7b820e6
VCUONG\VietCuong <vietpham460@gmail.com>
2019-01-05 22:05:52 +0700

    DAO, BUS: Mat Hang

--- DoAnUDQL1/Project/BUS/MatHangBUS.cs
+++ DoAnUDQL1/Project/BUS/MatHangBUS.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Project.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Project.BUS
 {
     class MatHangBUS
     {
+        //khởi tạo đối tượng khi gọi chỉ cần dùng toán tử . mà k cần khởi tạo biến
+        private static MatHangBUS instance;
+        public static MatHangBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MatHangBUS();
+                }
+                return instance;
+            }
+        }
+
+        public MATHANG GetByID(string MaMH)
+        {
+            return MatHangDAO.Instance.getById(MaMH);
+        }
+
+        public void GetAll(DataGridView dgv)
+        {
+            MatHangDAO.Instance.getAl(dgv);
+        }
+
+        public string CreatCode()
+        {
+            return MatHangDAO.Instance.CreateCode();
+        }
+
+        public void Insert(string TenMatHang, int SoLuongTon, int DonGia)
+        {
+            string MaMH = MatHangDAO.Instance.CreateCode();
+            MATHANG mh = new MATHANG();
+            mh.TenMatHang = TenMatHang;
+            mh.SoLuongTon = SoLuongTon;
+            mh.DonGia = DonGia;
+
+            MatHangDAO.Instance.Insert(mh);
+        }
+
+        public void Update(string MaMH, MATHANG mh)
+        {
+            MatHangDAO.Instance.Update(MaMH, mh);
+        }
     }
 }
--- DoAnUDQL1/Project/DAO/MatHangDAO.cs
+++ DoAnUDQL1/Project/DAO/MatHangDAO.cs
@@ -3,10 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Project.DAO
 {
     class MatHangDAO
     {
+        //khởi tạo đối tượng khi gọi chỉ cần dùng toán tử . mà k cần khởi tạo biến
+        private static MatHangDAO instance;
+
+        public static MatHangDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MatHangDAO();
+                }
+                return instance;
+            }
+        }
+
+        public void getAl(DataGridView dgv)
+        {
+            using (QLDLDataContext db = new QLDLDataContext())
+            {
+                var query = db.MATHANGs.Select(i => i).ToList();
+
+                dgv.DataSource = query;
+            }
+        }
+
+        public MATHANG getById(string id)
+        {
+            MATHANG item;
+
+            using(QLDLDataContext db = new QLDLDataContext())
+            {
+                item = db.MATHANGs.Where(i => i.MaMatHang == id).FirstOrDefault();
+            }
+
+            return item;
+        }
+
+        public string CreateCode()
+        {
+            string MaMH;
+            using (QLDLDataContext db = new QLDLDataContext())
+            {
+                List<string> lstMa = db.MATHANGs.Select(p => p.MaMatHang.Trim()).ToList();
+                MaMH = lstMa[0];
+                foreach (string temp in lstMa)
+                {
+                    if (temp.Length > MaMH.Length)
+                    {
+                        MaMH = temp;
+                    }
+                    else if (temp.Length == MaMH.Length)
+                    {
+                        if (String.Compare(temp.Substring(2, temp.Length - 2), MaMH.Substring(2, MaMH.Length - 2)) > 0)
+                        {
+                            MaMH = temp;
+                        }
+                    }
+                }
+                MaMH = MaMH.TrimEnd();
+                string x = MaMH.Substring(2);
+                string y = (int.Parse(x) + 1).ToString();
+                MaMH = "MH" + y;
+            }
+            return MaMH;
+        }
+
+        public void Insert(MATHANG mh)
+        {
+            using(QLDLDataContext db = new QLDLDataContext())
+            {
+                try
+                {
+                    db.MATHANGs.InsertOnSubmit(mh);
+                    db.SubmitChanges();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+        public void Update(string maMH, MATHANG mh)
+        {
+            using (QLDLDataContext db = new QLDLDataContext())
+            {
+                try
+                {
+                    var item = db.MATHANGs.Where(i => i.MaMatHang == maMH).FirstOrDefault();
+
+                    if (item != null)
+                    {
+                        item.TenMatHang = mh.TenMatHang;
+                        item.DonGia = mh.DonGia;
+                        item.SoLuongTon = mh.SoLuongTon;
+
+                        db.SubmitChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
     }
 }

ed49ddfd0494453d1fab19ba253cea8e017c738b
VCUONG\VietCuong <vietpham460@gmail.com>
2019-01-05 22:40:54 +0700

    DONVITINH: DAO, BUS

--- DoAnUDQL1/Project/BUS/DonViTinhBUS.cs
+++ DoAnUDQL1/Project/BUS/DonViTinhBUS.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Project.DAO;
 
 namespace Project.BUS
 {
-    class DonViTinhBUS
+    public class DonViTinhBUS
     {
+        //khởi tạo đối tượng khi gọi chỉ cần dùng toán tử . mà k cần khởi tạo biến
+        private static DonViTinhBUS instance;
+        public static DonViTinhBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DonViTinhBUS();
+                }
+                return instance;
+            }
+        }
     }
 }
--- DoAnUDQL1/Project/DAO/DonViTinhDAO.cs
+++ DoAnUDQL1/Project/DAO/DonViTinhDAO.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Project.DAO
 {
-    class DonViTinhDAO
+    public class DonViTinhDAO
     {
+        //khởi tạo đối tượng khi gọi chỉ cần dùng toán tử . mà k cần khởi tạo biến
+        private static DonViTinhDAO instance;
+        public static DonViTinhDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DonViTinhDAO();
+                }
+                return instance;
+            }
+        }
+
+        public void GetAll(DataGridView dgv)
+        {
+            using (QLDLDataContext db = new QLDLDataContext())
+            {
+                
+            }
+
+        }
+        public DONVITINH GetByID(string MaDL)
+        {
+            DONVITINH temp;
+            using (QLDLDataContext db = new QLDLDataContext())
+            {
+                
+            }
+            return temp;
+        }
+
     }
 }
